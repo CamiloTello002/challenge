05379b6436064ce0ac9ee3d0d200e2bb2f7daf6b
CamiloTello002 <camilojtello5@gmail.com>
2024-06-29 17:11:36 -0500

    Imported routers to app.js

--- api/app.js
+++ api/app.js
@@ -1,5 +1,10 @@
 const express = require('express')
 
+/** Importing routers from ./routes */
+const bookRouter = require('./routes/book.routes')
+const reservationRouter = require('./routes/reservation.routes')
+const usersRouter = require('./routes/users.routes')
+
 const app = express();
 
 app.use(express.json());
