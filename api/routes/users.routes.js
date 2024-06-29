4aaa7d42290cc103b862b77ad67478fadefc4f7f
CamiloTello002 <camilojtello5@gmail.com>
2024-06-29 17:09:40 -0500

    Added endpoint templates for all entities

--- api/routes/users.routes.js
+++ api/routes/users.routes.js
@@ -1,1 +1,14 @@
 const express = require('express');
+
+const router = express.Router();
+
+router.route('/')
+    .get()
+    .post();
+
+router.route('/:id')
+    .get()
+    .put()
+    .delete();
+
+module.exports = router;
