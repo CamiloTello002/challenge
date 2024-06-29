9796d7161c704a374fcac6b60cf984a5479dc339
CamiloTello002 <camilojtello5@gmail.com>
2024-06-29 16:41:31 -0500

    Modularized the files a little bit Created an app.js file for only app-specific code Created a server.js file that requires the app.js file. This is only in charge of server and database connections Added an .env file for port and MongoDB database URI

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
-const express = require('express')
+const app = require('./app')
+const mongoose = require('mongoose')
+const port = process.env.PORT || 5000;
 
-const app = express();
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log(`Database connected to ${process.env.MONGODB_URI}`);
+        app.listen(port, console.log(`Server started on port ${port}`));
 
-
-module.exports = app;
+    })
+    .catch((error) => console.log(error));
---
+++ client/todo/src/app/item.ts
@@ -0,0 +1,5 @@
+// An item is an object that contains a description and can be marked done
+export interface Item {
+    description: string,
+    done: boolean
+}
